8e6f9daa65acaa58d939e3e235aaf5432aa8c3f2
Berges <iraniber@hotmail.com>
2018-01-11 15:14:11 -0500

    New Neural Network Implementation

--- main.py
+++ main.py
@@ -12,33 +12,18 @@ training_input = df[df.out == "Hernia"].iloc[0:40, :6]
 training_input = training_input.append(df[df.out == "Spondylolisthesis"].iloc[0:100, :6], ignore_index=True)
 training_input = training_input.append(df[df.out == "Normal"].iloc[0:66, :6], ignore_index=True)
 
-training_input = (training_input.transform(lambda x: x/100)).values
-
 #Change Hernia to 1, Spondylolisthesis to 2, and Normal to 0
 df.loc[df.out == "Hernia", 'out'] = 1
 df.loc[df.out == "Spondylolisthesis", 'out'] = 2
 df.loc[df.out == "Normal", 'out'] = 0
+
 training_output = df.iloc[0:40, 6]
 training_output = training_output.append(df.iloc[61:161, 6])
 training_output = training_output.append(df.iloc[211:277, 6])
-training_output = training_output.values
-print(training_output)
 
-print(training_input)
+training_output = training_output.values
+
 input("Press any key to continue")
 
-# train neural network
-# demonstrate predictions
-SEED = 20180104
-ITERATION_COUNT = 5000
-DELTA = 0.02
-mini_medic = nn.NeuralNetwork(6, 1, SEED, 2)
-mini_medic2 = nn.NeuralNetwork(6, 1, SEED, 2)
-mini_medic.train(training_input, training_output, ITERATION_COUNT)
-mini_medic2.train(training_input, training_output, ITERATION_COUNT)
-test_inputs = df.iloc[1,:6]
-test_inputs = (test_inputs.transform(lambda x: x/100)).values
-print("Initital test", test_inputs)
-print(mini_medic.out_predict(test_inputs))
-print(mini_medic2.out_predict(test_inputs))
-input("Press any key to exit")
+MiniMedic = nn.NeuralNetwork()
+MiniMedic.normalize_training_input(training_input)
--- neural_network.py
+++ neural_network.py
@@ -1,77 +1,17 @@
 import numpy as np
+import pandas as pd
 
 class NeuralNetwork():
 
-    def __init__(self, input_count, output_count, seed, labels):
-        np.random.seed(seed)
-        self.input_count = input_count
-        self.output_count = output_count
-        self.labels = labels
-        self.generate_random_weights()
+    def __init__(self):
+        self.normal_contstants = []
 
-    def __sigmoid(self, x):
-        return (self.labels - 1) * (1 / (1 + np.exp(-x * (self.labels - 1))))
+    def normalize_training_input(self, training_input):
+        for column in training_input:
+            self.normal_contstants.append((training_input[column].max() - training_input[column].min())/2)
 
-    def __sigmoid_derivative(self, x):
-        return (self.labels - 1) * x * (1 - x)
+        for i in range(len(self.normal_contstants)):
+            distance = self.normal_contstants[i]
+            training_input.iloc[:, i] = (training_input.iloc[:, i].transform(lambda x: (x - distance)/distance))
 
-    def generate_random_weights(self):
-        self.synaptic_weights = np.random.uniform(0, (self.labels - 1), size=(self.input_count))
-        print("Initial synaptic weights: \n", self.synaptic_weights)
-
-    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
-        errors = []
-        weights = []
-
-        for iteration in range(number_of_training_iterations):
-
-            output = self.predict(training_set_inputs)
-            print("Output: \n", output)
-
-            error = training_set_outputs - output
-            errors.append(abs(np.mean(error)))
-
-            print("Error: \n", error)
-
-            adjustment = np.dot(training_set_outputs.T, error * self.__sigmoid_derivative(output))
-            print("Adjustment:")
-            input(adjustment)
-
-            self.synaptic_weights = self.synaptic_weights + adjustment
-            weights.append(self.synaptic_weights.copy())
-
-        return errors, weights
-
-    def train_until_fit(self, training_set_inputs, training_set_outputs, error_delta):
-
-        count = 0;
-
-        last_error_mean = 0
-
-        while(True):
-            output = self.predict(training_set_inputs)
-            error = training_set_outputs - output
-            error_mean = np.mean(error)
-            check = (sum(abs(error)))
-
-            if (check < error_delta):
-                print("Training is complete!")
-                print("Training took {0} iterations to get fit".format(count))
-                break
-            adjustments = np.dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
-            self.synaptic_weights += adjustments
-            count += 1
-
-        return count
-
-    def untrain(self):
-        self.generate_random_weights()
-
-    def out_predict(self, inputs):
-        print(inputs)
-        print(self.synaptic_weights)
-        print(np.dot(inputs, self.synaptic_weights))
-        return self.__sigmoid(np.dot(inputs, self.synaptic_weights))
-
-    def predict(self, inputs):
-        return self.__sigmoid(np.dot(inputs, self.synaptic_weights))
+        print(training_input)
